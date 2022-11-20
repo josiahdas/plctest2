954853b7c3e660216a45dcf73d8e90b54bc65afd
josiahdas <107151821+josiahdas@users.noreply.github.com>
2022-11-19 22:44:47 -0500

    Add files via upload

---
+++ testfile_with_error1.py
@@ -0,0 +1,11 @@
+
+##########
+#copy this code into the lex_analyzer.py scanner  
+###########
+
+10 * (1/0)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+while True print('Hello world')
+  File "<stdin>", line 1
+    while True print('Hello world')
