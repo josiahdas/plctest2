dda92efb7fc310404a1ca3102d1ccc393d70112c
josiahdas <107151821+josiahdas@users.noreply.github.com>
2022-11-19 22:52:52 -0500

    Add files via upload

---
+++ testfile_with_error2.py
@@ -0,0 +1,21 @@
+
+##########
+#copy this code into the lex_analyzer.py scanner  
+###########
+
+myfunction(x, y):
+    return x + y
+
+INCASE:
+    print("Hello!")
+
+INCASE mark >= 50
+    print("You passed!")
+
+INCASE arriving:
+    print("Hi!")
+esle:
+    print("Bye!")
+
+INCASE flag:
+print("Flag is set!")
